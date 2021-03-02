6886124cdb6a14df69f06613f2814e68c6c3a8f1
kairzhan <kairzhankural@gmail.com>
2021-03-02 19:02:31 +0600

    displaying news

---
+++ NewsAppMVVM/Cell/ArticleTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsAppMVVM
+//
+//  Created by kairzhan on 3/2/21.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
--- NewsAppMVVM/Controllers/NewsTableViewController.swift
+++ NewsAppMVVM/Controllers/NewsTableViewController.swift
@@ -12,6 +12,7 @@ import RxCocoa
 class NewsTableViewController: UITableViewController {
     
     let disposeBag = DisposeBag()
+    private var articleListVM: ArticleListViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +22,37 @@ class NewsTableViewController: UITableViewController {
         populateNews()
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM == nil ? 0: self.articleListVM.articlesVM.count
+    }
+    
     private func populateNews() {
         let resource = Resource<ArticleResponse>(url: URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=8b5efbb9647d421fa1596e6eda2ae78e")!)
         
         URLRequest.load(resource: resource)
-            .subscribe(onNext: {
-                print($0)
+            .subscribe(onNext: { articleResponse in
+                let articles = articleResponse.articles
+                self.articleListVM = ArticleListViewModel(articles: articles)
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }).disposed(by: disposeBag)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("Cell is not found")
+        }
+        
+        let articleVM = articleListVM.articleAt(index: indexPath.row)
+        articleVM.title.asDriver(onErrorJustReturn: "")
+            .drive(cell.titleLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        articleVM.description.asDriver(onErrorJustReturn: "")
+            .drive(cell.descriptionLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        return cell
     }
 }
