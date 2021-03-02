58d3a201745b9abd1ba66bed82a216388c418003
kairzhan <kairzhankural@gmail.com>
2021-03-02 18:12:37 +0600

    fetching news

--- NewsAppMVVM/Controllers/NewsTableViewController.swift
+++ NewsAppMVVM/Controllers/NewsTableViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class NewsTableViewController: UITableViewController {
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        populateNews()
+    }
+    
+    private func populateNews() {
+        let resource = Resource<ArticleResponse>(url: URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=8b5efbb9647d421fa1596e6eda2ae78e")!)
+        
+        URLRequest.load(resource: resource)
+            .subscribe(onNext: {
+                print($0)
+            }).disposed(by: disposeBag)
     }
 }
--- NewsAppMVVM/Models/Article.swift
+++ NewsAppMVVM/Models/Article.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct ActicleResponse: Decodable {
+struct ArticleResponse: Decodable {
     let articles: [Article]
 }
 
